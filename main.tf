d7c464804e5ea0728e5cde2804b8f272874a4d08
Максим Мельников <tryhardworker@mail.ru>
2024-09-06 22:50:26 +0300

    Add comments in code

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Create Virtual Private Cloud and subnetwork
 resource "yandex_vpc_network" "this" {
   name = "${var.name_prefix}-private"
 }
@@ -9,6 +10,7 @@ resource "yandex_vpc_subnet" "private" {
   network_id     = yandex_vpc_network.this.id
 }
 
+# Create disk and VM
 resource "yandex_compute_disk" "boot-disk" {
   name     = "${var.name_prefix}-boot-disk"
   zone     = var.zone
@@ -43,25 +45,30 @@ resource "yandex_compute_instance" "this" {
   }
 }
 
+# Create Yandex Managed Service for YDB
 resource "yandex_ydb_database_serverless" "this" {
   name = "${var.name_prefix}-test-ydb-serverless"
 }
 
+# Create service account
 resource "yandex_iam_service_account" "bucket" {
   name = "${var.name_prefix}-sa"
 }
 
+# Assign a role to a service account
 resource "yandex_resourcemanager_folder_iam_member" "storage_editor" {
   folder_id = var.folder_id
   role      = "storage.editor"
   member    = "serviceAccount:${yandex_iam_service_account.bucket.id}"
 }
 
+# Create static access key
 resource "yandex_iam_service_account_static_access_key" "this" {
   service_account_id = yandex_iam_service_account.bucket.id
   description        = "static access key for object storage"
 }
 
+# Create bucket
 resource "yandex_storage_bucket" "this" {
   bucket     = "terraform-bucket-${random_string.bucket_name.result}"
   access_key = yandex_iam_service_account_static_access_key.this.access_key
